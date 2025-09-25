16a30298b8bff7209594b9f2a7274513b1d1f17e
vasupradhasrinivasan01 <vasupradha.srinivasan01@gmail.com>
2025-09-25 12:37:16 +0530

    SQL query

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -26,13 +26,8 @@ query = "SELECT * FROM customers LIMIT 10;"
 customers_sample = pd.read_sql(query, conn)
 print(customers_sample)
 
-# Example 2: Get all sales for a specific CustomerID
-customer_id = 1234
-query = f"SELECT * FROM sales WHERE CustomerID = {customer_id};"
-sales_for_customer = pd.read_sql(query, conn)
-print(sales_for_customer)
 
-# Example 3: Get products with Price > 1000
+# Example 2: Get products with Price > 1000
 query = "SELECT ProductName, Price FROM products WHERE Price > 1000;"
 expensive_products = pd.read_sql(query, conn)
 print(expensive_products)
