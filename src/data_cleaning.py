529a5f93234300167bb57745454b8b82e0e83570
vasupradhasrinivasan01 <vasupradha.srinivasan01@gmail.com>
2025-10-03 16:56:49 +0530

    Modified dropna subset

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -10,8 +10,8 @@ df1.dropna()
 
 df1['Email'] = df1['Email'].str.replace(r'@.*','@email.com',regex=True)
 df1['Region'] = df1['Region'].fillna('Unknown')
-df1=df1.dropna(subset='Name')
-df1=df1.dropna(subset='Email')
+df1=df1.dropna(subset=['Name'])
+df1=df1.dropna(subset=['Email'])
 df1=df1[~df1['Name'].str.contains(r'Jr.|Sr.', case=False,na=False)]
 # print(df1)
 
@@ -41,7 +41,7 @@ df3.dropna()
 # print(df3.columns)
 df3 = df3.dropna(subset='ProductName')
 df3 = df3[~df3['ProductName'].str.contains(r'Plus | Pro' ,case=False,na=False)]
-df3=df3.dropna(subset='Price')
+df3=df3.dropna(subset=['Price'])
 df3['Category'] = df3['Category'].fillna('Accessories')
 
 df1.to_csv("data/generated_output/customers_data.csv", index=False)
